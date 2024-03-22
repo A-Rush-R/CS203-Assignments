00fe631f016bf4f28cc44dc6eae42c242bb845f0
Coder04rajan <118625483+Coder04rajan@users.noreply.github.com>
2024-03-22 12:38:46 +0530

    Handled case of input=100

--- Birthday_Paradox/birthday_paradox.py
+++ Birthday_Paradox/birthday_paradox.py
@@ -1,9 +1,7 @@
-p_limit = float(input("Enter probability that at least two students have the same birthday in %: "))
-
-while not (p_limit > 0 and p_limit < 100) :
-    print("The value must be between 0 and 100!")
+p_limit = float(input("Enter probability that at least two students have the same birthday in % : "))
+while not (p_limit >= 0 and p_limit <= 100) :
+    print("The value must be in the range [0,100] !")
     p_limit = float(input("Enter probability that at least two students have the same birthday in %: "))
-
 # Converts p into a decimal value
 p_coincident_birthday = p_limit/100
 
@@ -16,9 +14,11 @@ p_k = 1.0
 k = 1
 
 # Loop along the probability of this not happening
-while p_k > p_n:
+while ((p_k > p_n) and p_n!=0) :
     # if p_neg is still not small enough, we increase the class strength by one
     p_k = p_k * (365-k)/365
     k += 1
-
-print(k)
+# for the case when 100% proabability is required we can use PHP to say that it will be true for k=366. For k=365 it is still possible that every person has a different birthday
+if(p_n==0):
+    k=366
+print("The minimum number of students required will be: ", k)
