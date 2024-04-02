d2b32753ff6c432e45328e2f09bbb452136e6b60
A-Rush-A <arush.up@gmail.com>
2024-04-02 18:12:48 +0530

    removed old version and added saved figs

--- Generalized_Monty_Hall_Problem/monty.py
+++ Generalized_Monty_Hall_Problem/monty.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
 from matplotlib import cm
 from matplotlib.ticker import LinearLocator
 
 fig = plt.figure()
+fig.set_size_inches(18.5, 10.5)
 ax1 = fig.add_subplot(121, projection='3d')
 ax2 = fig.add_subplot(122, projection='3d')
 
@@ -13,19 +13,27 @@ def prob_ratio(n, k, num):
         return np.nan
     doors = ['C' for _ in range(k)]
     doors += ['G' for _ in range(n-k)]
-    # num = 5000
 
     win_og = 0
     win_sw = 0
+    choices = np.arange(0,n)
+    host_choices = np.arange(k,n)
     for _ in range(num):
-        choice = np.random.randint(0, n)
+        choices_curr = choices.copy()
+        host_choices_curr = host_choices.copy()
+        choice = np.random.choice(choices_curr)
         if doors[choice] == 'C':
             win_og += 1
-        if choice == n - 1 :
-            choice = n - 2
-        new_choice = np.random.randint(0, n - 1)
-        while new_choice == choice:
-            new_choice = np.random.randint(0, n - 1)
+        else : #chosen gate has a goat, so host has to pick a gate other than this gate
+            host_choices_curr = np.delete(host_choices, np.where(host_choices == choice))
+        # if choice == n - 1 :
+        #     choice = n - 2
+        host_choice = np.random.choice(host_choices_curr)
+        choices_curr = np.delete(choices_curr, np.where(choices_curr == choice))
+        choices_curr = np.delete(choices_curr, np.where(choices_curr == host_choice))
+        new_choice = np.random.choice(choices_curr)
+        # while new_choice == choice:
+        #     new_choice = np.random.randint(0, n - 1)
         if doors[new_choice] == 'C':
             win_sw += 1
         
@@ -42,7 +50,7 @@ def prob_ratio_exact(n, k):
 
 # Make data for the first surface.
 N = 100
-num = 1000
+num = 10000
 n = np.arange(3, N, 1)
 k = np.arange(1, N - 2, 1)
 n, k = np.meshgrid(n, k)
@@ -51,17 +59,20 @@ vectorized_prob_ratio_exact = np.vectorize(prob_ratio_exact)
 Z = vectorized_prob_ratio(n, k, num)
 
 # Plot the first surface.
-surf = ax1.plot_surface(n, k, Z, cmap=cm.coolwarm,
-                       linewidth=0, antialiased=False)
+surf = ax1.plot_surface(n, k, Z, cmap=cm.coolwarm,linewidth=0, antialiased=False)
 ax1.set_title('Plot from Simulation')
 
 # Make data for the second surface (initially set to a plane).
 Z_exact = vectorized_prob_ratio_exact(n, k)
 
 # Plot the second surface (plane).
-surf_plane = ax2.plot_surface(n, k, Z_exact, color='yellow', alpha=0.5)
-ax1.set_title('Plot from Exact Calculation')
+surf_plane = ax2.plot_surface(n, k, Z_exact,cmap=cm.coolwarm,linewidth=0, antialiased=False)
+ax2.set_title('Plot from Exact Calculation')
 
+
+# Set the same color map for both plots
+surf.set_cmap(cm.coolwarm)
+surf_plane.set_cmap(cm.coolwarm)
 # Customize the z axis.
 ax1.set_zlim(0.5, 2.5)
 ax1.zaxis.set_major_locator(LinearLocator(10))
@@ -80,4 +91,6 @@ ax2.set_zlabel('P_W/P_T')
 # Add a color bar which maps values to colors.
 fig.colorbar(surf, shrink=0.5, aspect=5)
 
+# plt.savefig("finalplot.png")
+plt.savefig("finalplot10k.png")
 plt.show()
